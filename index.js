c1b6ffdde410e9b4dc9a520c87e203424d0d5657
suryanshvermaa <154125921+suryanshvermaa@users.noreply.github.com>
2025-01-16 11:43:05 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-fs.readFile('csvjson.json', 'utf8', (err, data) => {
+fs.readFile('sponsersTesla.json', 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
@@ -11,18 +11,11 @@ fs.readFile('csvjson.json', 'utf8', (err, data) => {
         if(item.email){
             const obj={};
             obj.id=item.id;
-            obj.name=item.name;
-            obj.email=item.email;
             obj.company=item.company;
-            obj.contactNo=item.contactNo;
+            obj.email=item.email;
             updatedData.push(obj);
         }
     });
-    fs.writeFile('updatedData.json', JSON.stringify(updatedData), (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log('File has been created');
-    });
+    console.log(JSON.stringify(updatedData));
+    
 });
